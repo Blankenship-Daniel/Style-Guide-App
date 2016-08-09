3fcaad44e07b45be7ff1531c8fb318692caa66f3
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-08-09 12:09:25 -0600

    added functionality to be able to write JSON returned from the style guide parser to a file

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 var sgDriver = require('./js/classes/StyleGuideDriver.js');
 
 
 app.get('/', function(request, response) {
     var sg = new sgDriver.StyleGuideDriverNamespace.StyleGuideDriver();
     sg.run('_brand_common.scss', function(json) {
-        console.log(json);
+        fs.writeFile('sass.json', JSON.stringify(json), function(error) {
+            if (error) {
+                return console.log(err);
+            }
+
+            console.log('The file was saved!');
+        });
     });
 
     response.sendFile(path.join(__dirname + '/styleguide.html'));
